8f9301e29012e15d972e5f080347ca4387ebdab1
Jake Funke <jifunks@gmail.com>
2017-08-22 17:24:09 -0700

    Added GET to routings

--- app/routes/node_routes.js
+++ app/routes/node_routes.js
@@ -1,5 +1,19 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
 // in Express, routes are wrapped in a function which takes Express instance and database as arguments
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error': 'An error has occured'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+ 
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
