32b431e5ae3f47f7035d0412ec487f0002842a25
Jake Funke <jifunks@gmail.com>
2017-08-22 17:19:15 -0700

    Implement valid response

--- app/routes/node_routes.js
+++ app/routes/node_routes.js
@@ -1,8 +1,13 @@
 module.exports = function(app, db) {
 // in Express, routes are wrapped in a function which takes Express instance and database as arguments
   app.post('/notes', (req, res) => {
-    // create note here
-    console.log(req.body);
-    res.send('Hello');
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
   });
 };
